ec2b438065aaacc8993c334d96c0ff3e954bd73b
Siddharth Patel <sidharthpatel012@gmail.com>
2024-05-22 22:24:26 -0400

    Add: contains dummy transactions along with its payment status.

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+	{
+		productName: "Javascript  Tutorials",
+		productNumber: "85743",
+		paymentStatus: "Due",
+		status: "Pending",
+	},
+	{
+		productName: "CSS Full Course",
+		productNumber: "97245",
+		paymentStatus: "Refunded",
+		status: "Declined",
+	},
+	{
+		productName: "Flex-Box Tutorials",
+		productNumber: "36452",
+		paymentStatus: "Paid",
+		status: "Active",
+	},
+];
